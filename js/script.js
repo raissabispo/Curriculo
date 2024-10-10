3edd8a5927014172fd3ee179b6d3617340abb8c2
Raissa Vitória <123013855+raissabispo@users.noreply.github.com>
2024-10-10 18:51:38 +0000

    Atualização do formulário

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function enviar() {
+    
+    event.preventDefault();
+
+    let nome = document.getElementById('nome').value;
+    let sobrenome = document.getElementById('sobrenome').value;
+    let telefone = document.getElementById('telefone').value;
+    let email = document.getElementById('email').value;
+    let mensagem = document.getElementById('mensagem').value;
+    let lingua = document.getElementById('lingua').value;
+    let prototipo = document.getElementById('prototipo').value;
+    let linguagens = document.getElementById('linguagens').selectedOptions;
+    let banco = document.querySelector('input[name="banco"]:checked');
+    let turno = document.getElementById('turno').value;
+    let horario = document.getElementById('horario').value;
+    let data = document.getElementById('data').value;
+
+  
+    if (nome === '' || sobrenome === '' || telefone === '' || email === '' || mensagem === '' || lingua === '' || prototipo === '' || linguagens.length === 0 || !banco || turno === '' || horario === '' || data === '') {
+      alert('Por favor, preencha todos os campos obrigatórios!');
+      return false;
+    }
+    document.getElementById('mensagemEnviar').innerHTML = 'Formulário enviado com sucesso!';
+    }
--- script.js
+++
@@ -1,5 +0,0 @@
-
-function enviar(){
-    let mensagemp = document.getElementById("mensagemp");
-    mensagemp.innerHTML = " Formulário enviado com sucesso!";
-}
