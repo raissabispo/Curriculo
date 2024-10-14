3da44d7c52afc3219e4d5617eeb45d3c0189a3eb
Raissa Vitória <123013855+raissabispo@users.noreply.github.com>
2024-10-14 00:49:42 +0000

    Alteração formulário

--- js/script.js
+++ js/script.js
@@ -1,27 +1,32 @@
 function enviar(event) {
-    
-    event.preventDefault();
+  event.preventDefault(); // Previne o envio padrão do formulário
 
-    let nome = document.getElementById('nome').value;
-    let sobrenome = document.getElementById('sobrenome').value;
-    let telefone = document.getElementById('telefone').value;
-    let email = document.getElementById('email').value;
-    let mensagem = document.getElementById('mensagem').value;
-    let lingua = document.getElementById('lingua').value;
-    let prototipo = document.getElementById('prototipo').value;
-    let linguagens = document.getElementById('linguagens').selectedOptions;
-    let banco = document.querySelector('input[name="banco"]:checked');
-    let turno = document.getElementById('turno').value;
-    let horario = document.getElementById('horario').value;
-    let data = document.getElementById('data').value;
+  // Capturando valores dos campos
+  let nome = document.getElementById('nome').value;
+  let sobrenome = document.getElementById('sobrenome').value;
+  let telefone = document.getElementById('telefone').value;
+  let email = document.getElementById('email').value;
+  let mensagem = document.getElementById('mensagem').value;
+  let lingua = document.getElementById('lingua').value;
+  let linguagens = document.getElementById('linguagens').selectedOptions;
+  let banco = document.querySelector('input[name="banco"]:checked');
+  let turnos = document.querySelectorAll('input[name="turno"]:checked');
+  let horario = document.getElementById('horario').value;
+  let data = document.getElementById('data').value;
 
-      if (nome === '' || sobrenome === '' || telefone === '' || email === '' || mensagem === '' || turno === ''|| data === '') {
-        alert('Por favor, preencha todos os campos obrigatórios!');
-        return false;
-      }
+  // Verificação dos campos obrigatórios
+  if (nome === '' || sobrenome === '' || telefone === '' || email === '' || mensagem === '' || turnos.length === 0 || data === '') {
+      alert('Por favor, preencha todos os campos obrigatórios!');
+      return false;
+  }
 
-      document.getElementById('mensagemEnviar').innerHTML = 'Formulário enviado com sucesso!';
-      setTimeout(function() {
-        document.getElementById('mensagemEnviar').innerHTML = '';
-      }, 5000);
-    }
+  // Se tudo estiver correto, exibe a mensagem de sucesso
+  document.getElementById('mensagemEnviar').innerHTML = 'Formulário enviado com sucesso!';
+
+  // Limpa a mensagem após 5 segundos
+  setTimeout(function() {
+      document.getElementById('mensagemEnviar').innerHTML = '';
+  }, 5000);
+
+  return true; // Validação concluída com sucesso
+}
