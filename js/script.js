162830861d1647509ea032578dad0a2627de7847
Raissa Vitória <123013855+raissabispo@users.noreply.github.com>
2024-10-14 00:33:15 +0000

    Alteração formulário

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function enviar() {
+function enviar(event) {
     
     event.preventDefault();
 
@@ -24,32 +24,4 @@ function enviar() {
       setTimeout(function() {
         document.getElementById('mensagemEnviar').innerHTML = '';
       }, 5000);
-=======
-
-  event.preventDefault(); 
-
-  let nome = document.getElementById('nome').value;
-  let sobrenome = document.getElementById('sobrenome').value;
-  let telefone = document.getElementById('telefone').value;
-  let email = document.getElementById('email').value;
-  let mensagem = document.getElementById('mensagem').value;
-  let lingua = document.getElementById('lingua').value;
-  let linguagens = document.getElementById('linguagens').selectedOptions;
-  let banco = document.querySelector('input[name="banco"]:checked');
-  let turnos = document.querySelectorAll('input[name="turno"]:checked');
-  let horario = document.getElementById('horario').value;
-  let data = document.getElementById('data').value;
-
-  if (nome === '' || sobrenome === '' || telefone === '' || email === '' || mensagem === '' ||  turnos.length === 0 || data === '') {
-      alert('Por favor, preencha todos os campos obrigatórios!');
-      return false;
-  }
-  document.getElementById('mensagemEnviar').innerHTML = 'Formulário enviado com sucesso!';
-
-  setTimeout(function() {
-      document.getElementById('mensagemEnviar').innerHTML = '';
-  }, 5000);
-
-  return true; 
-}
->>>>>>> b907c6b (Atualização do formulário)
+    }
